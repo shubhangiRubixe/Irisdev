c44fbaf335beed377e97a716b749c0bb9ed1ec36
Shubhangi Sakarkar <shubhangi@rubixe.com>
2022-11-24 18:05:10 +0530

    The app is ready

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ model=pickle.load(open('lr.pkl','rb'))
 def homepage():
     return render_template('home.html')
 
+## from postman
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
@@ -20,6 +21,15 @@ def predict_api():
     output=model.predict(np.array(list(data.values())).reshape(1,-1))
     return jsonify(output[0])
 
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    data=[float(x)for x in request.form.values()]
+    final_input=np.array(data).reshape(1,-1)
+    print(final_input)
+    output1=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The predicted iris species is {}".format(output1))
 
 if __name__=='__main__':
     app.run(debug=True)
